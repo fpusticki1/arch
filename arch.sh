88351e7c53ce0ad18a3e6476ea4c20b7c3c20f46
Franjo Pusticki <47257897+fpusticki1@users.noreply.github.com>
2022-01-21 10:05:44 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Arch Linux Installer by: Franjo Pusticki
 # ------------------------------------------------------------------------------
@@ -6,14 +6,14 @@ echo
 echo "####################################################"
 echo "##### Welcome to the Arch installation script. #####"
 echo "####################################################"
-echo && sleep 2
+echo && sleep 1
+
 
 ###-----------------------------------------------------------------------------
 ### PART 1: BASIC CONFIGURATION ------------------------------------------------
 ###-----------------------------------------------------------------------------
 
 #SET KEYMAP
-echo "Setting keymap..." && sleep 1
 loadkeys croat
 
 #USER INPUTS
@@ -25,8 +25,8 @@ read -p "*** Connect to wifi? (y/n): " wifi
 if [ "${wifi}" = "y" ]; then
   iwctl device list
   read -p "*** Enter your device name: " mydevice
-  iwctl station ${mydevice} scan
-  iwctl station ${mydevice} get-networks
+  iwctl station ${mydevice} scan && sleep 3
+  iwctl station ${mydevice} get-networks && sleep 3
   read -p "*** Enter your wifi name: " mynetwork
   read -p "*** Enter your wifi password: " mywifipassword
   echo "Connecting to wifi network..." && sleep 2
@@ -37,7 +37,6 @@ else
 fi
 
 #SET TIME
-echo "Setting timezone..." && sleep 1
 timedatectl set-timezone Europe/Zagreb
 timedatectl set-ntp true
 
@@ -45,23 +44,22 @@ timedatectl set-ntp true
 echo
 echo "*****************  Listing Disk drives  *****************"
 echo "*********************************************************" && sleep 1
-lsblk
-echo
-echo "*********************************************************" && sleep 1
 fdisk -l
 echo "*********************************************************" && sleep 1
 echo
 read -p "This operation will erase the disk!!!
-*** Enter your disk name (example: /dev/sda): " mydisk
+*** Enter your disk name (example: /dev/sda ): " mydisk
+echo
 read -p "Selected disk is: *** ${mydisk} ***
 *** Are you sure you want to erase it and install Arch Linux? (YES/n): " confirm
 if [ "${confirm}" = "YES" ]; then
   for n in ${mydisk}* ; do umount $n ; done
   for n in ${mydisk}* ; do swapoff $n ; done
-  (echo g; echo n; echo; echo; echo +512M; echo Y; echo t; echo 1; echo w) | fdisk ${mydisk} #boot partition
-  (echo n; echo; echo; echo +4G; echo Y; echo t; echo; echo 19; echo w) | fdisk ${mydisk} #swap partition
-  (echo n; echo; echo; echo; echo Y; echo w) | fdisk ${mydisk} #root partition
-  read -p "*** Press Enter to continue..." continue
+  wipefs -a "${mydisk}" "${mydisk}1" "${mydisk}2" "${mydisk}3"
+  (echo g; echo n; echo; echo; echo +512M; echo t; echo 1; echo w) | fdisk ${mydisk} #boot partition
+  (echo n; echo; echo; echo +4G; echo t; echo; echo 19; echo w) | fdisk ${mydisk} #swap partition
+  (echo n; echo; echo; echo; echo w) | fdisk ${mydisk} #root partition
+  read -p "*** Disk erased and partitions created. Press Enter to continue..." continue
   bootpart="${mydisk}1"
   swappart="${mydisk}2"
   rootpart="${mydisk}3"
@@ -72,28 +70,28 @@ else
 fi
 
 #PARTITION FORMATTING
-echo "Formatting partitions..." && sleep 1
 mkfs.ext4 ${rootpart}
 mkswap ${swappart}
 mkfs.fat -F 32 ${bootpart}
 
 #MOUNTING FILESYSTEMS
-echo "Mounting filesystems..." && sleep 1
 mount ${rootpart} /mnt
 swapon ${swappart}
 mkdir /mnt/boot
 mount ${bootpart} /mnt/boot
-read -p "*** Press Enter to continue..." continue
 
 #SHOW CREATED PARTITIONS
 echo
 echo "*****************  Listing Disk drives  *****************"
 echo "*********************************************************" && sleep 1
-lsblk
-echo
-echo "*********************************************************" && sleep 1
 fdisk -l
 echo "*********************************************************" && sleep 1
+echo
+read -p "Please check your new partitions.
+*** Press Enter to continue..." continue
+
+lsblk
+echo
 echo
 read -p "Please check your new partitions.
 *** Press Enter to continue..." continue
