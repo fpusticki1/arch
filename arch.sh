643e5dab0aeb6697d8d1d560d5abff4a4eac82f9
Franjo Pusticki <47257897+fpusticki1@users.noreply.github.com>
2024-01-24 09:56:31 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -11,20 +11,20 @@ echo "####################################################"
 ### SET KEYMAP
 loadkeys croat
 
-### USER INPUTS
-echo "***************************************************"
-myname="Franjo Pustički"
-read -p "*** Is this a laptop? (y/n): " iflaptop
-read -p "*** Select CPU? (intel/amd): " mycpu
-read -p "*** Select GPU? (intel/nvidia): " mygpu
-read -p "*** Enter hostname: " myhostname
-read -p "*** Enter username: " myuser
-read -p "*** Enter password: " mypassword
-read -p "*** Install NTH apps? (y/N): " nth
-read -p "*** Install Thunderbird? (y/N): " thund
-read -p "*** Install Printer support? (y/N): " print
-read -p "*** Install Torrent support? (y/N): " torr
-read -p "*** Install Plex media server? (y/N): " plex
+### ### USER INPUTS
+### echo "***************************************************"
+### myname="Franjo Pustički"
+### read -p "*** Is this a laptop? (y/n): " iflaptop
+### read -p "*** Select CPU? (intel/amd): " mycpu
+### read -p "*** Select GPU? (intel/nvidia): " mygpu
+### read -p "*** Enter hostname: " myhostname
+### read -p "*** Enter username: " myuser
+### read -p "*** Enter password: " mypassword
+### read -p "*** Install NTH apps? (y/N): " nth
+### read -p "*** Install Thunderbird? (y/N): " thund
+### read -p "*** Install Printer support? (y/N): " print
+### read -p "*** Install Torrent support? (y/N): " torr
+### read -p "*** Install Plex media server? (y/N): " plex
 
 ### SET TIME
 timedatectl set-timezone Europe/Zagreb
@@ -36,20 +36,24 @@ echo "*****************  Listing Disk drives  *****************"
 echo "*********************************************************"
 fdisk -l
 echo "*********************************************************"
-echo && sleep 1
+echo
 echo "This operation will erase the disk!!!"
-echo && sleep 1
+echo
 read -p "*** Enter your disk name (example: /dev/nvme0n1 ): " mydisk
 echo
 read -p "Selected disk is: *** ${mydisk} ***
 *** Are you sure you want to erase it and install Arch Linux? (yes/n): " confirm
 if [ "${confirm}" = "yes" ]; then
+  umount "${mydisk}p1" "${mydisk}p2" "${mydisk}p3"
   sgdisk -Z ${mydisk}
   sleep 1
   (echo n; echo; echo; echo +512M; echo ef00; echo n; echo; echo; echo; echo 8300; echo w; echo y) | gdisk ${mydisk}
   sleep 1
   bootpart="${mydisk}p1"
   rootpart="${mydisk}p2"
+else
+  exit 0
+fi
 
 ### PARTITION FORMATTING
 mkfs.ext4 ${rootpart}
